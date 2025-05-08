4b77118d045bd7414ba9239af71f973688ec2e63
Murat Alpaslan <muratalpaslan@windowslive.com>
2025-05-08 13:00:02 +0300

    merge için kontrol noktası oluşturuldu

--- OnlineSinavSistemi.Core/obj/Debug/net9.0/OnlineSinavSistemi.Core.AssemblyInfo.cs
+++ OnlineSinavSistemi.Core/obj/Debug/net9.0/OnlineSinavSistemi.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineSinavSistemi.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00c0faca3ae8bba9befb32a00b97a7854ae1a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879a27395f6e70f93b6bb80dd868e54f49300268")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineSinavSistemi.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineSinavSistemi.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineSinavSistemi.DataAccess/obj/Debug/net9.0/OnlineSinavSistemi.DataAccess.AssemblyInfo.cs
+++ OnlineSinavSistemi.DataAccess/obj/Debug/net9.0/OnlineSinavSistemi.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineSinavSistemi.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00c0faca3ae8bba9befb32a00b97a7854ae1a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879a27395f6e70f93b6bb80dd868e54f49300268")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineSinavSistemi.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineSinavSistemi.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
